dea8c70ac289abbc0ad983a2afbfc3d6c4f488ea
xieyoub <596931725@qq.com>
2016-12-19 22:29:01 +0800

    添加菜单窗口类等资源文件
    如题

---
+++ ColdEyes/UI/Wnd/MyMenuWnd.cpp
@@ -0,0 +1,191 @@
+#include "stdafx.h"
+#include "MyMenuWnd.h"
+#include "conio.h"
+
+
+CMyMenuWnd::CMyMenuWnd()
+{
+}
+
+CMyMenuWnd::~CMyMenuWnd()
+{
+}
+
+
+LPCTSTR CMyMenuWnd::GetWindowClassName() const
+{
+	return _T("MenuWnd");
+}
+
+CDuiString CMyMenuWnd::GetSkinFile()
+{
+	return _T("MenuUI.xml");
+}
+
+void CMyMenuWnd::Notify(TNotifyUI& msg)
+{
+
+}
+
+
+
+LRESULT CMyMenuWnd::HandleCustomMessage(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled)
+{
+	bHandled = FALSE;
+	return 0;
+}
+
+// UI Init 
+void CMyMenuWnd::InitWindow()
+{
+	//CKeyBoardUI* pLayout =static_cast<CKeyBoardUI*> (m_pm.FindControl(_T("keyboard")));
+	//pLayout->OnEvent += MakeDelegate(this, &CMyMenuWnd::OnLayoutDoEventTest);
+	//LayoutInit();
+	InitMenuItem();
+
+	//°ó¶¨´¬Ãû±à¼­¿òµÄÊäÈë¼üÅÌ
+	static_cast<CKeyBoardUI*>(m_pm.FindControl(kKeyboardName))->SetShowStringControl(m_pm.FindControl(kEditCtlHostNameName)); 
+
+	MakeItemsDelegate();
+}
+
+void CMyMenuWnd::InitMenuItem()
+{
+	//Host Set
+	SetSubMenuParent(kSubMenuItemHostName, kMenuItemHostSetName);
+	SetSubMenuParent(kSubMenuItemSysSetName,kMenuItemHostSetName);
+
+	//Auot Watch
+	SetSubMenuParent(kSubMenuItemAwTimeName, kMenuItemAutoWatchName);
+	SetSubMenuParent(kSubMenuItemAlarmVoiceName, kMenuItemAutoWatchName);
+	SetSubMenuParent(kSubMenuItemAlarmLightName, kMenuItemAutoWatchName);
+	SetSubMenuParent(kSubMenuItemAwOnOffRecordName, kMenuItemAutoWatchName);
+}
+
+void CMyMenuWnd::LayoutInit()
+{
+	AddLayout(_T("HostName.xml"), (CContainerUI*)m_pm.FindControl(kBodyLayoutHostSetName));
+	AddLayout(_T("SysSet.xml"), (CContainerUI*)m_pm.FindControl(kBodyLayoutHostSetName));
+
+	AddLayout(_T("AwTime.xml"), (CContainerUI*)m_pm.FindControl(kBodyLayoutAwTime));
+	AddLayout(_T("AlarmVoice.xml"), (CContainerUI*)m_pm.FindControl(kBodyLayoutAwTime));
+	AddLayout(_T("AlarmLights.xml"), (CContainerUI*)m_pm.FindControl(kBodyLayoutAwTime));
+	AddLayout(_T("AwOnOffRecords.xml"), (CContainerUI*)m_pm.FindControl(kBodyLayoutAwTime));
+	//AddLayout(_T(""))
+}
+
+void CMyMenuWnd::AddLayout(LPCTSTR xmlName, CContainerUI* ParentLayout)
+{
+	CDialogBuilder Builder;
+	ParentLayout->Add((CContainerUI*)Builder.Create(xmlName, NULL, this, &m_pm, NULL));
+}
+
+void CMyMenuWnd::AddAtLayout(LPCTSTR xmlName, CContainerUI* ParentLayout, UINT8 inx)
+{
+	CDialogBuilder Builder;
+	ParentLayout->AddAt((CContainerUI*)Builder.Create(xmlName, NULL, this, &m_pm, NULL),inx);
+}
+
+void CMyMenuWnd::SetSubMenuParent(LPCTSTR sSubMenuName, LPCTSTR sParentName)
+{
+	static_cast<CSubMenuItemUI*>(m_pm.FindControl(sSubMenuName))->SetParentMenuItem(sParentName);
+}
+
+void CMyMenuWnd::MakeItemsDelegate()
+{
+	SubMenuMakeDelegate(kSubMenuItemHostName);
+	SubMenuMakeDelegate(kSubMenuItemSysSetName);
+	SubMenuMakeDelegate(kSubMenuItemAwTimeName);
+	SubMenuMakeDelegate(kSubMenuItemAlarmVoiceName);
+	SubMenuMakeDelegate(kSubMenuItemAlarmLightName);
+	SubMenuMakeDelegate(kSubMenuItemAwOnOffRecordName);
+
+}
+
+void CMyMenuWnd::SubMenuMakeDelegate(const TCHAR* const Name)
+{
+	static_cast<CSubMenuItemUI*>(m_pm.FindControl(Name))->OnEvent += MakeDelegate(this, &CMyMenuWnd::OnSubMenuItem);
+}
+
+
+// Delegate Function
+bool CMyMenuWnd::OnSubMenuItem(void * param)
+{
+	TEventUI* pMsg = (TEventUI*)param;
+	CSubMenuItemUI* item = (CSubMenuItemUI*)pMsg->pSender;
+	CDuiString Name = item->GetName();
+	switch (pMsg->Type) {
+	case UIEVENT_KEYDOWN:
+		switch (pMsg->wParam) {
+		case VK_BACK:
+			if (item->GetParentItem())
+				item->GetParentItem()->SetFocus();
+			break;
+
+		case VK_RIGHT:
+			if (_tcsicmp(Name, kSubMenuItemHostName) == 0) {
+
+			}
+			else if (_tcsicmp(Name, kSubMenuItemSysSetName) == 0) {
+
+			}
+			else if (_tcsicmp(Name, kSubMenuItemAwTimeName) == 0) {
+
+			}
+			else if (_tcsicmp(Name, kSubMenuItemAlarmVoiceName) == 0) {
+
+			}
+			else if (_tcsicmp(Name, kSubMenuItemAlarmLightName) == 0) {
+
+			}
+			else if (_tcsicmp(Name, kSubMenuItemAwOnOffRecordName) == 0) {
+
+			}
+			return false;
+			break;
+
+		}
+		break;
+	}
+	return true;
+}
+
+
+// SubMenuItem Add Or Delete
+void CMyMenuWnd::AddAlarmMenuItem()
+{
+
+}
+
+void CMyMenuWnd::AddVideoObtainMenuItem()
+{
+
+}
+
+void CMyMenuWnd::AddPortConfigMenuItem()
+{
+
+}
+
+void CMyMenuWnd::DeleteAlarmMenuItem()
+{
+}
+
+void CMyMenuWnd::DeleteVideoObtainMenuItem()
+{
+}
+
+void CMyMenuWnd::DeletePortConfigMenuItem()
+{
+}
+
+
+//bool CMyMenuWnd::OnLayoutDoEventTest(void * param)
+//{
+//	TEventUI* pMsg = (TEventUI*)param;
+//
+//	if (pMsg->Type == UIEVENT_KEYDOWN) {
+//		Print("GG ---------");
+//	}
+//	return false;
+//}
---
+++ ColdEyes/UI/Wnd/MyMenuWnd.h
@@ -0,0 +1,82 @@
+#pragma once
+
+#include "DuiLib/UIlib.h"
+
+#include <list>
+#include "UI\Control\EditExUI.h"
+#include "UI\Control\SubMenuItemUI.h"
+#include "UI\Control\KeyBoardUI.h"
+#include "UI\Control\SliderExUI.h"
+#include "UI\Control\LabelExUI.h"
+#include "UI\Control\TimeSpanPickerUI.h"
+
+//	Parent MenuItem
+const TCHAR* const kMenuItemAlarmVideoName = _T("parent_menu_alarm_video");
+const TCHAR* const kMenuItemHostSetName = _T("parent_menu_host_set");
+const TCHAR* const kMenuItemAutoWatchName = _T("parent_menu_auto_watch");
+const TCHAR* const kMenuItemVideoObtainName = _T("parent_menu_video_obtain");
+const TCHAR* const KMenuItemHomeWatchName = _T("parent_menu_home_watch");
+
+//SubMenu
+const TCHAR* const kSubMenuItemHostName = _T("host_name");
+const TCHAR* const kSubMenuItemSysSetName = _T("sys_set");
+
+const TCHAR* const kSubMenuItemAwTimeName = _T("aw_time");
+const TCHAR* const kSubMenuItemAlarmVoiceName = _T("alarm_voice");
+const TCHAR* const kSubMenuItemAlarmLightName = _T("alarm_light");
+const TCHAR* const kSubMenuItemAwOnOffRecordName = _T("aw_on_off_record");
+
+// BodyLayout
+const TCHAR* const kBodyLayoutAlarmVideo = _T("body_alarm_video");
+const TCHAR* const kBodyLayoutHostSetName = _T("body_host_set");
+const TCHAR* const kBodyLayoutAwTime = _T("body_auto_watch");
+const TCHAR* const kBodyLayoutVideoObtain = _T("body_video_obtain");
+
+
+//host name
+const TCHAR* const kEditCtlHostNameName = _T("edit_host_name");
+const TCHAR* const kKeyboardName = _T("keyboard");
+
+
+using namespace DuiLib;
+
+
+class CMyMenuWnd
+	: public WindowImplBase
+{
+public:
+	CMyMenuWnd();
+	~CMyMenuWnd();
+
+	LPCTSTR GetWindowClassName() const;
+	virtual CDuiString GetSkinFile();
+	void Notify(TNotifyUI& msg);
+	LRESULT HandleCustomMessage(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled);
+	virtual void InitWindow();
+	void InitMenuItem();
+	void LayoutInit();
+	void AddLayout(LPCTSTR xmlName,CContainerUI* ParentLayout);
+	void AddAtLayout(LPCTSTR xmlName, CContainerUI* ParentLayout,UINT8 inx);
+	void SetSubMenuParent(LPCTSTR sSubMenuName, LPCTSTR sParentName);
+
+	void MakeItemsDelegate();	//控件委托函数初始化
+	void SubMenuMakeDelegate(const TCHAR* const Name);	//二级子控件委托
+
+	// item Delegate
+	bool OnSubMenuItem(void*param);
+
+	//Add SubMenuItem
+	void  AddAlarmMenuItem();		//报警视频
+	void  AddVideoObtainMenuItem();	//视频调取
+	void  AddPortConfigMenuItem();	//摄像头设置
+
+	//Delete SubMenuItem
+	void  DeleteAlarmMenuItem();
+	void  DeleteVideoObtainMenuItem();
+	void  DeletePortConfigMenuItem();
+
+private:
+	int mDPI;
+};
+
+
